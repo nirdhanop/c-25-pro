66f1f3f9e492bedaeb9eb2d7dde6af1ce5c1c8ba
nirdhanop <60476755+nirdhanop@users.noreply.github.com>
2020-10-11 09:38:55 +0530

    Add files via upload

--- dustbin.js
+++ dustbin.js
@@ -1,23 +1,21 @@
 class Dustbin
 {
-
-	constructor(x,y,width,height)
-	{
-		var options={
-
-			isStatic:true			
-      }
-      this.body=Bodies.rectangle(x, y, width, height , options);
-      this.width= width;
-      this.height = height;
-    
+        constructor(x,y,width,height)
+    {
+	var options={
+                isStatic:true			
+                    }
+        this.body=Bodies.rectangle(x, y, width, height , options);
+        this.width= width;
+        this.height = height;
+        this.image = loadImage("images/dustbingreen.png");
       
- 		World.add(world, this.body);
+        World.add(world, this.body);
 
 	}
 	display(){
 
-        //again just the regular coe
+        //again just the regular code
         var pos =this.body.position;
         var angle = this.body.angle;
         push();
@@ -26,6 +24,8 @@ class Dustbin
         rectMode(CENTER);
         fill(0);
         rect(0, 0, this.width, this.height);
+        imageMode(CENTER);
+        image(this.image,0,0,this.width,this.height);
         pop();
 	}
 
